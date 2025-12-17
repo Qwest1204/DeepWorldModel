d5c3d3f3973b82b1c396a6cc5aec64405d7b6065
Qwest1204 <myPCwithUbuntu@gmail.com>
2025-12-17 14:40:21 +0300

    add layer to vae

--- models/vae.py
+++ models/vae.py
@@ -116,13 +116,15 @@ class ResVAE(nn.Module):
             ResDownBlock(channels, 32, stride=2),
             ResDownBlock(32, 64, stride=2),
             ResDownBlock(64, 128, stride=2),
-            ResDownBlock(128, 512, stride=2),
+            ResDownBlock(128, 256, stride=2),
+            ResDownBlock(256, 512, stride=2),
             ResDownBlock(512, hidden_dim, stride=2),
         )
 
         self.decoder = nn.Sequential(
             ResUpBlock(hidden_dim, 512, stride=2),
-            ResUpBlock(512, 128, stride=2),
+            ResUpBlock(512, 256, stride=2),
+            ResUpBlock(256, 128, stride=2),
             ResUpBlock(128, 64, stride=2),
             ResUpBlock(64, 32, stride=2),
             ResUpBlock(32, channels, stride=2),
